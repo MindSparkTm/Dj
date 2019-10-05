8ca196d6c3a9e1b104e942394262f7c080678a19
surajt <smartsurajit2008@gmail.com>
2019-10-05 11:20:07 -0400

    added fileupload model for bulk upload of books

--- library/admin.py
+++ library/admin.py
@@ -2,37 +2,45 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Category,Author,Publisher,Book,Review
+from .models import Category, Author, Publisher, Book, Review, FileUpload
+
+
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     list_filter = ('name', 'last_modified')
-    readonly_fields = ('created','last_modified')
+    readonly_fields = ('created', 'last_modified')
     search_fields = ('name',)
 
+
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name','last_name','email')
+    list_display = ('first_name', 'last_name', 'email')
     list_filter = ('email', 'last_modified')
-    readonly_fields = ('created','last_modified')
-    search_fields = ('first_name','email')
+    readonly_fields = ('created', 'last_modified')
+    search_fields = ('first_name', 'email')
+
 
 class PublisherAdmin(admin.ModelAdmin):
-    list_display = ('name','address','state','country')
-    list_filter = ('name', 'country','last_modified')
-    readonly_fields = ('created','last_modified')
-    search_fields = ('name','country')
+    list_display = ('name', 'address', 'state', 'country')
+    list_filter = ('name', 'country', 'last_modified')
+    readonly_fields = ('created', 'last_modified')
+    search_fields = ('name', 'country')
+
 
 class BookAdmin(admin.ModelAdmin):
-    def get_publisher(self,obj):
+    def get_publisher(self, obj):
         return obj.publisher.name
-    list_display = ('title','get_publisher')
-    list_filter = ('title', 'last_modified')
-    readonly_fields = ('created','last_modified')
-    search_fields = ('name','get_publisher')
 
-admin.site.register(Category,CategoryAdmin)
-admin.site.register(Author,AuthorAdmin)
-admin.site.register(Publisher,PublisherAdmin)
-admin.site.register(Book,BookAdmin)
+    list_display = ('title', 'get_publisher')
+    list_filter = ('title', 'last_modified')
+    readonly_fields = ('created', 'last_modified')
+    search_fields = ('name', 'get_publisher')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Review)
+admin.site.register(FileUpload)
--- library/models.py
+++ library/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from user.models import CustomUser
 from django.urls import reverse
+
+
 # Create your models here.
 
 class TimeStampedModel(models.Model):
@@ -14,24 +16,24 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
+
 class Category(TimeStampedModel):
-    name = models.CharField(_('Category Name'),max_length=50)
+    name = models.CharField(_('Category Name'), max_length=50)
 
     class Meta:
         verbose_name = ('Category')
         verbose_name_plural = ('Categories')
 
-
     def __unicode__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('library:category_detail_view',kwargs={'pk':self.pk})
+        return reverse('library:category_detail_view', kwargs={'pk': self.pk})
 
 
 class Author(TimeStampedModel):
-    first_name = models.CharField(_('First Name'),max_length=50)
-    last_name = models.CharField(_('Last Name'),max_length=50)
+    first_name = models.CharField(_('First Name'), max_length=50)
+    last_name = models.CharField(_('Last Name'), max_length=50)
     email = models.EmailField()
 
     class Meta:
@@ -39,49 +41,49 @@ class Author(TimeStampedModel):
         verbose_name_plural = ('Authors')
 
     def __str__(self):
-        return u'{}{}'.format(self.first_name,self.last_name)
+        return u'{}{}'.format(self.first_name, self.last_name)
 
     def __unicode__(self):
-        return u'{}{}'.format(self.first_name,self.last_name)
+        return u'{}{}'.format(self.first_name, self.last_name)
 
     def get_absolute_url(self):
-        return reverse('library:author_detail',kwargs={'pk':self.pk})
+        return reverse('library:author_detail', kwargs={'pk': self.pk})
 
 
 class Publisher(TimeStampedModel):
-    name = models.CharField(_('Publisher Name'),max_length=50)
-    address = models.CharField(_('Address'),max_length=100)
-    state = models.CharField(_('State'),max_length=50)
-    city = models.CharField(_('City'),max_length=50)
-    country = models.CharField(_('Country'),max_length=60)
+    name = models.CharField(_('Publisher Name'), max_length=50)
+    address = models.CharField(_('Address'), max_length=100)
+    state = models.CharField(_('State'), max_length=50)
+    city = models.CharField(_('City'), max_length=50)
+    country = models.CharField(_('Country'), max_length=60)
 
     def __unicode__(self):
         return self.name
+
 
 class BookManager(models.Manager):
 
     def get_book_by_id(self, book_id):
         return self.get_queryset().filter(pk=book_id)
 
-class Book(TimeStampedModel):
 
-    title = models.CharField(_('Title of Book'),max_length=100)
+class Book(TimeStampedModel):
+    title = models.CharField(_('Title of Book'), max_length=100)
     authors = models.ManyToManyField(Author)
-    book_cover_image = models.ImageField(upload_to='images/',blank=True,null=True)
-    publisher = models.ForeignKey(Publisher,on_delete=models.CASCADE)
+    book_cover_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     publication_date = models.DateField()
     categories = models.ForeignKey(Category)
-    read_by = models.ManyToManyField(CustomUser,related_name='books')
-
+    read_by = models.ManyToManyField(CustomUser, related_name='books')
 
     def __unicode__(self):
-        return u'{}{}'.format(self.title,self.publication_date)
-        
+        return u'{}{}'.format(self.title, self.publication_date)
+
     def get_absolute_url(self):
-        return reverse('library:book_detail',kwargs={'pk':self.pk})
+        return reverse('library:book_detail', kwargs={'pk': self.pk})
 
     @classmethod
-    def get_book_by_id(self,pk):
+    def get_book_by_id(self, pk):
         try:
             book = Book.objects.get(id=pk)
         except Exception as ex:
@@ -96,8 +98,19 @@ class Book(TimeStampedModel):
 
 class Review(TimeStampedModel):
     comment = models.TextField(_('Enter your comment'))
-    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    book = models.ForeignKey(Book,on_delete=models.CASCADE,related_name='reviews')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
 
     def __unicode__(self):
         return self.comment
+
+
+class FileUpload(TimeStampedModel):
+    document = models.FileField(upload_to='bulk_upload/')
+    file_name = models.CharField(_('Add a file name'), max_length=50)
+
+    def __str__(self):
+        return u'{}'.format(self.file_name)
+
+    def __unicode__(self):
+        return u'{}'.format(self.file_name)
