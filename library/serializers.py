3a25f5e661b43bed76c6784818c660ff1551dbc5
surajt <smartsurajit2008@gmail.com>
2019-10-12 03:39:51 -0400

    modified serializer

--- library/serializers.py
+++ library/serializers.py
@@ -9,9 +9,8 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = ( 'first_name', 'last_name')
 
 class BookSerializer(serializers.ModelSerializer):
-    publisher_name = serializers.ReadOnlyField()
     authors = AuthorSerializer(read_only=True,many=True)
 
     class Meta:
         model = Book
-        fields = ('title', 'authors', 'categories', 'publication_date', 'book_cover_image', 'publisher_name',)
+        fields = ('title', 'authors', 'category', 'publication_date', 'book_cover_image', 'publisher',)
